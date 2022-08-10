779652ba59a9b38ca7ea96537e0acf663dbf3ef6
酷达舒 <kucs@isyscore.com>
2022-08-10 14:38:16 +0800

    完善curl命令并添加了ping命令实现

--- curl/curl.go
+++ curl/curl.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
+	"github.com/robfig/cron"
 	"io"
 	"io/ioutil"
 	"log"
@@ -15,6 +17,13 @@ import (
 	"time"
 )
 
+var client = &http.Client{
+	Timeout: 5 * time.Second,
+}
+
+func init() {
+	client.Timeout = 1 * time.Minute
+}
 func main() {
 	headers := make(map[string][]string)
 	forms := make(map[string][]string)
@@ -25,6 +34,7 @@ func main() {
 	requestUrl := flag.String("h", "", "request url")
 	timeout := flag.Int("t", 5000, "request time out,unit is ms ,default 5000 ms")
 	responseHeader := flag.Bool("i", true, "println response header,default true")
+	job := flag.String("job", "", "crontab")
 	flag.Parse()
 
 	args := flag.Args()
@@ -66,6 +76,8 @@ func main() {
 			case "--data-binary":
 				dataBinary = arg
 				log.Panic("暂不支持", dataBinary)
+			case "--help":
+				usage()
 			default:
 				if *requestUrl == "" {
 					requestUrl = &arg
@@ -79,38 +91,49 @@ func main() {
 		log.Panic("url不能为空")
 	}
 	timeoutDuration, _ := time.ParseDuration(strconv.Itoa(*timeout) + "ms")
-	client := &http.Client{
-		Timeout: timeoutDuration,
-	}
+	client.Timeout = timeoutDuration
 	if url, err := url2.Parse(*requestUrl); err != nil {
 		log.Panicf("url[%s] unavaliable", url)
 	} else {
-		request, _ := http.NewRequest(*method, *requestUrl, strings.NewReader(dataRaw))
-		request.Form = forms
-		if dataRaw != "" {
-			headers["Content-Type"] = []string{"application/json"}
+		if job != nil && *job != "" {
+			ch := make(chan error, 1)
+			go func(method, requestUrl, dataRaw string, forms, headers map[string][]string, isMultiForm, responseHeader bool) {
+				starCron(*job, func() {
+					doRequest(method, requestUrl, dataRaw, forms, headers, isMultiForm, responseHeader)
+				})
+			}(*method, *requestUrl, dataRaw, forms, headers, isMultiForm, *responseHeader)
+			<-ch
 		}
-		if len(forms) > 0 {
-			headers["Content-Type"] = []string{"application/x-www-form-urlencoded"}
-		}
+		doRequest(*method, *requestUrl, dataRaw, forms, headers, isMultiForm, *responseHeader)
+	}
+}
 
-		request.Header = headers
+func doRequest(method, requestUrl, dataRaw string, forms, headers map[string][]string, isMultiForm, responseHeader bool) {
+	request, _ := http.NewRequest(method, requestUrl, strings.NewReader(dataRaw))
+	request.Form = forms
+	if dataRaw != "" {
+		headers["Content-Type"] = []string{"application/json"}
+	}
+	if len(forms) > 0 {
+		headers["Content-Type"] = []string{"application/x-www-form-urlencoded"}
+	}
 
-		if isMultiForm {
-			multipartRequest(request, forms)
-		}
-		if resp, err := client.Do(request); err != nil {
-			log.Panicf("url[%s] error,%v", url, err)
-		} else {
-			//打印请求头
-			if *responseHeader {
-				for k, v := range resp.Header {
-					log.Println(k, ":", v)
-				}
+	request.Header = headers
+
+	if isMultiForm {
+		multipartRequest(request, forms)
+	}
+	if resp, err := client.Do(request); err != nil {
+		log.Panicf("url[%s] error,%v", requestUrl, err)
+	} else {
+		//打印请求头
+		if responseHeader {
+			for k, v := range resp.Header {
+				log.Println(k, ":", v)
 			}
-			data, _ := ioutil.ReadAll(resp.Body)
-			log.Printf("%s", data)
 		}
+		data, _ := ioutil.ReadAll(resp.Body)
+		log.Printf("%s", data)
 	}
 }
 
@@ -134,4 +157,23 @@ func multipartRequest(request *http.Request, forms map[string][]string) {
 		_ = fileWriter.WriteField(key, val[0])
 	}
 	request.Header.Set("Content-Type", fileWriter.FormDataContentType())
+}
+
+func starCron(crontab string, handler func()) *cron.Cron {
+	job := cron.New()
+	job.AddFunc(crontab, handler)
+	job.Start()
+	return job
+}
+
+func usage() {
+	fmt.Println(
+		`用法: curl [-i] [-X GET|POST|PUT|DELETE ...] [-job * */1 * * * ?] [-t 5000] [-h 'Content-Type:application/json']
+            选项:
+               -t			超时时间，单位ms,默认5000ms
+				-X 			请求方法，默认GET，
+				-job		定时任务处理cron表示
+				-i			打印详细响应头信息
+`,
+	)
 }
