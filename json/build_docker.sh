3d73b0f46920ed08fc7871a9739e2bc73f04a2d8
酷达舒 <kucs@isyscore.com>
2022-12-29 12:02:55 +0800

    [update] 完善build脚本

--- json/build_docker.sh
+++ json/build_docker.sh
@@ -2,11 +2,11 @@ TAG=${1:-latest}
 
 echo "TAG=$TAG"
 
-echo "docker build -t 10.30.30.22:9080/isyscore/engine/isc-json-engine:$TAG ."
+echo "docker build -t 10.30.30.36:9080/isyscore/engine/isc-json-engine:$TAG ."
 
 docker build -t 10.30.30.36:9080/isyscore/engine/isc-json-engine:$TAG .
 
-echo "docker push 10.30.30.22:9080/isyscore/engine/isc-json-engine:$TAG"
+echo "docker push 10.30.30.36:9080/isyscore/engine/isc-json-engine:$TAG"
 
 docker push 10.30.30.36:9080/isyscore/engine/isc-json-engine:$TAG
 
